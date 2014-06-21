11d3c1a754883ea8829b2694ce6020ce1b4ca50d
hrk fkm <fkm@fkmsoft.jp>
2014-06-21 17:17:19 +0900

    Initial import

---
+++ KiiLib-Objective-C/KiiLib-Objective-C/KiiLib_Objective_C.h
@@ -0,0 +1,13 @@
+//
+//  KiiLib_Objective_C.h
+//  KiiLib-Objective-C
+//
+//  Created by fkm on 2014/06/21.
+//  Copyright (c) 2014年 fkmsoft.jp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KiiLib_Objective_C : NSObject
+
+@end
